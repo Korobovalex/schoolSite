58bb3e172bf7d61d537b958c4fdbd156c5c4cf9d
korobov Alex <korobovalex@mail.ru>
2022-07-13 18:27:23 +0300

    +Testimonials section

--- dev/js/main.js
+++ dev/js/main.js
@@ -21,3 +21,20 @@ faqs.forEach(faq => {
         }
     })
 });
+
+<!-- Initialize Swiper -->
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+      breakpoints: {
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        }
+
+      }
+  });
