0afc4650a90fe342a6793a0063e1f5a17e2adfc5
korobov Alex <korobovalex@mail.ru>
2022-07-13 13:54:47 +0300

    +FAQ section

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,5 +1,23 @@
 //    Navbar style on scrolling
 
 window.addEventListener('scroll', () => {
-   document.querySelector('nav').classList.toggle('scrolled', scrollY > 0)
+    document.querySelector('nav').classList.toggle('scrolled', scrollY > 0)
+});
+
+
+//    Show/hide FAQ answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+        //   toggle icon
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus';
+        } else {
+            icon.className = 'uil uil-plus';
+        }
+    })
 });
